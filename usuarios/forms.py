3094717cfe710554486e020d09587f6f32bbf31b
Jesus Andres Salazar Anacona <167454911+enalabanzaygracia@users.noreply.github.com>
2024-05-08 09:36:33 -0500

    Update forms.py
    COMENTARIOS

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,17 +1,21 @@
+# Importa validadores de Django.
 from django.core import validators
+# Importa clases y formularios necesarios de Django.
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+# Importa modelos de la aplicación usuarios.
 from usuarios.models import UserProfile, Rol, TipoSolicitud, Detallesolicitud, Tipodocumento
-
-
+# Define un formulario de registro de usuario que hereda de UserCreationForm de Django.
 class RegisterForm(UserCreationForm):
+    # Define campos adicionales para el formulario de registro.
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-    class Meta:
+        class Meta:
+        # Utiliza el modelo User de Django y define los campos a mostrar en el formulario.
         model = User
-        fields = ['username',  'first_name', 'last_name','email', 'password1', 'password2']
+        fields = ['username',  'first_name', 'last_name', 'email', 'password1', 'password2']
         labels = {
             'username': 'Numero de Identificacion',
             'password1': 'Contraseña',
@@ -20,44 +24,41 @@ class RegisterForm(UserCreationForm):
             'last_name': 'Apellido',
             'email': 'Correo Electronico',
             'rol': 'Rol',
-
-
-
         }
         widgets = {
+            # Define el estilo del campo en el formulario.
             'username': forms.TextInput(attrs={'class': 'form-control'}),
-        
             'first_name': forms.TextInput(attrs={'class': 'form-control'}),
             'last_name': forms.TextInput(attrs={'class': 'form-control'}),
-            
-
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
             'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
         }
-
+# Define un formulario para el rol del usuario.
 class RolForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         roles = kwargs.pop('roles', None)
         super(RolForm, self).__init__(*args, **kwargs)
         self.fields['id_rol'].label = 'Rol'
-    class Meta:
+        class Meta:
         model = UserProfile
         fields = ['id_rol']
-
+# Define un formulario para el tipo de documento del usuario.
 class DocForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         doc = kwargs.pop('tipodoc', None)
         super(DocForm, self).__init__(*args, **kwargs)
         self.fields['id_doc'].label = 'Tipodocumento'
-    class Meta:
+        class Meta:
         model = UserProfile
         fields = ['id_doc']
+# Define un formulario para el tipo de solicitud.
 class TipoSoliForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         tpsoli = kwargs.pop('tiposoli', None)
         super(TipoSoliForm, self).__init__(*args, **kwargs)
         self.fields['id_tiposolicitud'].label = 'TipoSolicitud'
-    class Meta:
+        class Meta:
         model = Detallesolicitud
         fields = ['id_tiposolicitud']
+
