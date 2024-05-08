d02557a58c8ae19137d8924470c782ccfd6af2f3
Jesus Andres Salazar Anacona <167454911+enalabanzaygracia@users.noreply.github.com>
2024-05-08 10:55:59 -0500

    Update views.py
    COMENTARIOS

--- coordinacion/views.py
+++ coordinacion/views.py
@@ -1,4 +1,4 @@
-# Importación de las funciones y clases necesarias desde los módulos de Django
+                                                                                # Importación de las funciones y clases necesarias desde los módulos de Django
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
@@ -6,29 +6,21 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from usuarios.models import Detallesolicitud
 from django.urls import reverse
-
-# Definición de las vistas
-
-# Vista para la página de inicio de coordinación
+                                                                                        # Definición de las vistas
+                                                                                        # Vista para la página de inicio de coordinación
 def inicio_coordinacion(request):
-    # Renderiza la plantilla 'inicio_coordinacion.html' con el título de la ventana
+                                                                                        # Renderiza la plantilla 'inicio_coordinacion.html' con el título de la ventana
     return render(request, 'vistas/inicio_coordinacion.html', {
         'tituloventana': "Inicio"
     })
+def noticias(request):                                                                   # Vista para la página de noticias
+      return render(request, 'vistas/noticias.html')                                     # Renderiza la plantilla 'noticias.html'
 
-# Vista para la página de noticias
-def noticias(request):
-    # Renderiza la plantilla 'noticias.html'
-    return render(request, 'vistas/noticias.html')
+def actualizarperfil(request):                                                          # Vista para la página de actualización de perfil
+       return render(request, 'vistas/actualizarperfil.html')                           # Renderiza la plantilla 'actualizarperfil.html'
 
-# Vista para la página de actualización de perfil
-def actualizarperfil(request):
-    # Renderiza la plantilla 'actualizarperfil.html'
-    return render(request, 'vistas/actualizarperfil.html')
+def gestiondepermisos(request):                                                         # Vista para la página de gestión de permisos
+       return render(request, 'vistas/gestiondepermisos.html')
 
-# Vista para la página de gestión de permisos
-def gestiondepermisos(request):
-    # Renderiza la plantilla 'gestiondepermisos.html'
-    return render(request, 'vistas/gestiondepermisos.html')
 
 
