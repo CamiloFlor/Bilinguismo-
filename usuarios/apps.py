578ba982b1d624b2528f4f33fee74e704505b1ff
Jesus Andres Salazar Anacona <167454911+enalabanzaygracia@users.noreply.github.com>
2024-05-08 09:39:21 -0500

    Update apps.py
    COMENTARIOS

--- usuarios/apps.py
+++ usuarios/apps.py
@@ -1,6 +1,4 @@
-from django.apps import AppConfig
-
-
-class UsuariosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'usuarios'
+from django.apps import AppConfig                                        # Importa la clase AppConfig de Django.
+class UsuariosConfig(AppConfig):                                         # Define una configuración para la aplicación 'usuarios'.
+    default_auto_field = 'django.db.models.BigAutoField'                 # Define el campo de auto-generación predeterminado para modelos de la base de datos.
+    name = 'usuarios'                                                    # Define el nombre de la aplicación.
