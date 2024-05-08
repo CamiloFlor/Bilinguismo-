822092b7e85ed2b3bf4dc1bf321a9603bdb8c830
Jesus Andres Salazar Anacona <167454911+enalabanzaygracia@users.noreply.github.com>
2024-05-08 08:43:19 -0500

    Update url.py
    COMENTARIOS

--- coordinacion/url.py
+++ coordinacion/url.py
@@ -1,19 +1,13 @@
-# Importación de las funciones y clases necesarias desde los módulos de Django y la aplicación coordinacion
+                                                                                                    # Importación de las funciones y clases necesarias desde los módulos de Django y la aplicación coordinacion
 from django.urls import path, include
 from coordinacion import views
 from .views import actualizarperfil, gestiondepermisos
-
-# Establecimiento del espacio de nombres de la aplicación
+                                                                                                    # Establecimiento del espacio de nombres de la aplicación
 app_name = 'coordinacion'
-
-# Definición de las URL de la aplicación
+                                                                                                    # Definición de las URL de la aplicación
 urlpatterns = [
-  
-    path('inicio-coordinacion/', views.inicio_coordinacion, name="inicio_coordinacion"),  # Ruta para la vista de inicio de coordinación
-    
-    path('noticias/', views.noticias, name="noticias"),# Ruta para la vista de noticias
-   
-    path('actualizarperfil/', views.actualizarperfil, name='actualizarperfil'), # Ruta para la vista de actualización de perfil
-
-    path('gestiondepermisos/', views.gestiondepermisos, name='gestiondepermisos'),    # Ruta para la vista de gestión de permisos
+      path('inicio-coordinacion/', views.inicio_coordinacion, name="inicio_coordinacion"),          # Ruta para la vista de inicio de coordinación
+      path('noticias/', views.noticias, name="noticias"),                                           # Ruta para la vista de noticias
+      path('actualizarperfil/', views.actualizarperfil, name='actualizarperfil'),                   # Ruta para la vista de actualización de perfil
+      path('gestiondepermisos/', views.gestiondepermisos, name='gestiondepermisos'),                # Ruta para la vista de gestión de permisos
 ]
